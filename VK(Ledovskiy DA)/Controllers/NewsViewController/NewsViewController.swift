06197a14fd51613aee4f17f98c7efd2eab1dddb6
DenisLedovskiy <BigDen_07@bk.ru>
2022-02-01 19:24:14 +0300

    Понял в чем была ошибка с сервисом работы с сетью. Исправил

--- VK(Ledovskiy DA)/Controllers/NewsViewController/NewsViewController.swift
+++ VK(Ledovskiy DA)/Controllers/NewsViewController/NewsViewController.swift
@@ -21,7 +21,7 @@ class NewsViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
 
     let session = Session.instance
-    var newsManager: NewsManager!
+    var newsManager = NewsManager()
 
     var newsItemsPostArray = [NewsItems]()
     var newsProfilesPostArray = [NewsProfiles]()
@@ -58,7 +58,7 @@ class NewsViewController: UIViewController {
 
         DispatchQueue.main.async { [self] in
 
-            get { feed in
+            newsManager.get { feed in
                 newsItemsPostArray = (feed?.response.items)!
                 newsProfilesPostArray = (feed?.response.profiles)!
                 newsGroupsPostArray = (feed?.response.groups)!
@@ -70,73 +70,73 @@ class NewsViewController: UIViewController {
         tableView.dataSource = self
     }
 
-    func get (_ completion: @escaping (NewsInitialResponse?) -> ()) {
-
-        AF.request("https://api.vk.com/method/newsfeed.get", parameters: [
-            "v": "5.81",
-            "access_token": session.token,
-            "count": 5,
-            "filters": "post"
-        ]).responseData { response in
-
-            guard let data = response.data else {return}
-
-            let decoder = JSONDecoder()
-            let json = JSON(data)
-            let dispatchGroup = DispatchGroup()
-
-            let vkItemsJSONArr = json["response"]["items"].arrayValue
-            let vkProfilesJSONArr = json["response"]["profiles"].arrayValue
-            let vkGroupsJSONArr = json["response"]["groups"].arrayValue
-
-            var vkItemArray: [NewsItems] = []
-            var vkProfilesArray: [NewsProfiles] = []
-            var vkGroupsArray: [NewsGroups] = []
-
-            DispatchQueue.global().async(group: dispatchGroup) {
-                for (index, item) in vkItemsJSONArr.enumerated() {
-                    do {
-                        let decodedItem = try decoder.decode(NewsItems.self, from: item.rawData())
-                        vkItemArray.append(decodedItem)
-
-                    } catch(let errorDecode) {
-                        print("Item desoding error at index \(index), err: \(errorDecode)")
-                    }
-                }
-            }
-
-            DispatchQueue.global().async(group: dispatchGroup) {
-                for (index, item) in vkProfilesJSONArr.enumerated() {
-                    do {
-                        let decodedItem = try decoder.decode(NewsProfiles.self, from: item.rawData())
-                        vkProfilesArray.append(decodedItem)
-
-                    } catch(let errorDecode) {
-                        print("Item desoding error at index \(index), err: \(errorDecode)")
-                    }
-                }
-            }
-
-            DispatchQueue.global().async(group: dispatchGroup) {
-                for (index, item) in vkGroupsJSONArr.enumerated() {
-                    do {
-                        let decodedItem = try decoder.decode(NewsGroups.self, from: item.rawData())
-                        vkGroupsArray.append(decodedItem)
-
-                    } catch(let errorDecode) {
-                        print("Item desoding error at index \(index), err: \(errorDecode)")
-                    }
-                }
-            }
-
-            dispatchGroup.notify(queue: DispatchQueue.main) {
-                let response = NewsResponse(items: vkItemArray,
-                                            profiles: vkProfilesArray,
-                                            groups: vkGroupsArray)
-                let feed = NewsInitialResponse(response: response)
-
-                completion(feed)
-            }
-        }
-    }
+//    func get (_ completion: @escaping (NewsInitialResponse?) -> ()) {
+//
+//        AF.request("https://api.vk.com/method/newsfeed.get", parameters: [
+//            "v": "5.81",
+//            "access_token": session.token,
+//            "count": 5,
+//            "filters": "post"
+//        ]).responseData { response in
+//
+//            guard let data = response.data else {return}
+//
+//            let decoder = JSONDecoder()
+//            let json = JSON(data)
+//            let dispatchGroup = DispatchGroup()
+//
+//            let vkItemsJSONArr = json["response"]["items"].arrayValue
+//            let vkProfilesJSONArr = json["response"]["profiles"].arrayValue
+//            let vkGroupsJSONArr = json["response"]["groups"].arrayValue
+//
+//            var vkItemArray: [NewsItems] = []
+//            var vkProfilesArray: [NewsProfiles] = []
+//            var vkGroupsArray: [NewsGroups] = []
+//
+//            DispatchQueue.global().async(group: dispatchGroup) {
+//                for (index, item) in vkItemsJSONArr.enumerated() {
+//                    do {
+//                        let decodedItem = try decoder.decode(NewsItems.self, from: item.rawData())
+//                        vkItemArray.append(decodedItem)
+//
+//                    } catch(let errorDecode) {
+//                        print("Item desoding error at index \(index), err: \(errorDecode)")
+//                    }
+//                }
+//            }
+//
+//            DispatchQueue.global().async(group: dispatchGroup) {
+//                for (index, item) in vkProfilesJSONArr.enumerated() {
+//                    do {
+//                        let decodedItem = try decoder.decode(NewsProfiles.self, from: item.rawData())
+//                        vkProfilesArray.append(decodedItem)
+//
+//                    } catch(let errorDecode) {
+//                        print("Item desoding error at index \(index), err: \(errorDecode)")
+//                    }
+//                }
+//            }
+//
+//            DispatchQueue.global().async(group: dispatchGroup) {
+//                for (index, item) in vkGroupsJSONArr.enumerated() {
+//                    do {
+//                        let decodedItem = try decoder.decode(NewsGroups.self, from: item.rawData())
+//                        vkGroupsArray.append(decodedItem)
+//
+//                    } catch(let errorDecode) {
+//                        print("Item desoding error at index \(index), err: \(errorDecode)")
+//                    }
+//                }
+//            }
+//
+//            dispatchGroup.notify(queue: DispatchQueue.main) {
+//                let response = NewsResponse(items: vkItemArray,
+//                                            profiles: vkProfilesArray,
+//                                            groups: vkGroupsArray)
+//                let feed = NewsInitialResponse(response: response)
+//
+//                completion(feed)
+//            }
+//        }
+//    }
 }
