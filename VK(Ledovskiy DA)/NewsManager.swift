875b9940d1826d33eab86e6d7c86ae6b8d7d1447
DenisLedovskiy <BigDen_07@bk.ru>
2022-02-01 19:14:57 +0300

    Сделал параллельный парсинг по примеру

--- VK(Ledovskiy DA)/NewsManager.swift
+++ VK(Ledovskiy DA)/NewsManager.swift
@@ -1,74 +1,17 @@
 //
-//  NewsViewController.swift
+//  NewsManager.swift
 //  VK(Ledovskiy DA)
 //
-//  Created by Денис Ледовский on 31.10.2021.
+//  Created by Денис Ледовский on 01.02.2022.
 //
 
-import UIKit
+import Foundation
 import Alamofire
 import SwiftyJSON
 
-enum PostCellType: Int, CaseIterable {
-    case author = 0
-    case text
-    case photo
-    case likeCount
-}
-
-class NewsViewController: UIViewController {
-
-    @IBOutlet weak var tableView: UITableView!
+class NewsManager {
 
     let session = Session.instance
-    var newsManager: NewsManager!
-
-    var newsItemsPostArray = [NewsItems]()
-    var newsProfilesPostArray = [NewsProfiles]()
-    var newsGroupsPostArray = [NewsGroups]()
-    
-    var newsArray = [News]()
-    let reuseIdentifierNews = "reuseIdentifierNews"
-    let reuseIdentifierAvatar = "reuseIdentifierAvatar"
-    let reuseIdentifierTextNews = "reuseIdentifierTextNews"
-    let reuseIdentifierImageConetnt = "reuseIdentifierImageConetnt"
-    let reuseIdentifierLikes = "reuseIdentifierLikes"
-
-    func fillNewsArray () {
-        let news1 = News(avatarNews: UIImage(named: "meganAva")!, titleNews: "Меган Фокс", textNews: "Теперь у меня есть замечательные котята)", contentImageNews: UIImage(named: "news1")!, counterLikeNews: "100")
-        let news2 = News(avatarNews: UIImage(named: "ronaldoAva")!, titleNews: "Криштиано Роналдо", textNews: "Зацените мою новую тачку!", contentImageNews: UIImage(named: "news2")!, counterLikeNews: "57")
-
-        newsArray.append(news1)
-        newsArray.append(news2)
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        fillNewsArray()
-        tableView.register(UINib(nibName: "NewsTableViewCell", bundle: nil),
-                           forCellReuseIdentifier: reuseIdentifierNews)
-        tableView.register(UINib(nibName: "Avatar+Name", bundle: nil),
-                           forCellReuseIdentifier: reuseIdentifierAvatar)
-        tableView.register(UINib(nibName: "TextNews", bundle: nil),
-                           forCellReuseIdentifier: reuseIdentifierTextNews)
-        tableView.register(UINib(nibName: "ImageContentNews", bundle: nil),
-                           forCellReuseIdentifier: reuseIdentifierImageConetnt)
-        tableView.register(UINib(nibName: "LikesCell", bundle: nil),
-                           forCellReuseIdentifier: reuseIdentifierLikes)
-
-        DispatchQueue.main.async { [self] in
-
-            get { feed in
-                newsItemsPostArray = (feed?.response.items)!
-                newsProfilesPostArray = (feed?.response.profiles)!
-                newsGroupsPostArray = (feed?.response.groups)!
-                tableView.reloadData()
-                print(newsItemsPostArray)
-            }
-        }
-        tableView.delegate = self
-        tableView.dataSource = self
-    }
 
     func get (_ completion: @escaping (NewsInitialResponse?) -> ()) {
 
@@ -139,4 +82,5 @@ class NewsViewController: UIViewController {
             }
         }
     }
+    
 }
